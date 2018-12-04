cb5b38f0e690940f37f7b60fb0281cd97dacf264
Fabian Thommen <ft@taywa.ch>
2018-12-04 17:57:08 +0100

    replace mysqli_error

--- tracenoizer.net/public_html/navV3.php
+++ tracenoizer.net/public_html/navV3.php
@@ -59,7 +59,7 @@ want more clones, run TraceNoizer again. <br>
 
 				$get = 'select usr_id from users where usr_name="'.$usr_name.'"';
 
-				$result = mysqli_query($connection, $get) or die (mysql_error());
+				$result = mysqli_query($connection, $get) or die (mysqli_error());
 
 				$usr_exist = mysqli_fetch_row($result);
 
@@ -127,9 +127,9 @@ want more clones, run TraceNoizer again. <br>
 
 				//echo $put;
 
-				//mysqli_query($connection, 'update users set usr_name="'.$usr_name.'",usr_pw="'.$usr_pw.'",usr_mail="'.$usr_mail.'" where usr_id='.$usr_id.'') or die (mysql_error());
+				//mysqli_query($connection, 'update users set usr_name="'.$usr_name.'",usr_pw="'.$usr_pw.'",usr_mail="'.$usr_mail.'" where usr_id='.$usr_id.'') or die (mysqli_error());
 
-				mysqli_query($connection, $put) or die (mysql_error());
+				mysqli_query($connection, $put) or die (mysqli_error());
 
 				
 
